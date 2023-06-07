b46cff730994151919d3e0135843dce968ea75fb
cjimenezlp <59283443+cjimenezlp@users.noreply.github.com>
2023-06-07 13:07:54 +0200

    Add files via upload

---
+++ ejercicio05.sql/src/main/java/com/ejercicio05/sql/ejercicio05/sql/Application.java
@@ -0,0 +1,13 @@
+package com.ejercicio05.sql.ejercicio05.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ ejercicio05.sql/src/test/java/com/ejercicio05/sql/ejercicio05/sql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio05.sql.ejercicio05.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
