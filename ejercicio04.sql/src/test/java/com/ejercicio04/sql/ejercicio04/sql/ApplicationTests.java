298737ad7379973f55685f535c2ba41cde75b90a
cjimenezlp <59283443+cjimenezlp@users.noreply.github.com>
2023-06-07 10:17:49 +0200

    Add files via upload

---
+++ ejercicio04.sql/src/test/java/com/ejercicio04/sql/ejercicio04/sql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio04.sql.ejercicio04.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
